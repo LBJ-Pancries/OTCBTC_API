77328d88a4fcbc1ccbd9bc59b4d6f16f8e9d1f36
liubingjie <breeder1121@gmail.com>
2018-03-26 17:57:15 +0800

    eos_cny

--- app/controllers/tickers_controller.rb
+++ app/controllers/tickers_controller.rb
@@ -7,21 +7,16 @@ class TickersController < ApplicationController
     ticker = Ticker.find(params[:id])
     response = RestClient.get "https://bb.otcbtc.com/api/v2/tickers"
     data = JSON.parse(response.body)
-    data.each do |key, value|
-      existing_ticker = Ticker.find_by_ticker_id(key)
-      next unless existing_ticker.nil?
-      ticker_data = value['ticker']
-      Ticker.update( ticker_id: key,
-                      at: value['at'],
-                      buy: ticker_data['buy'],
-                      sell: ticker_data['sell'],
-                      low: ticker_data['low'],
-                      high: ticker_data['high'],
-                      last: ticker_data['last'],
-                      vol: ticker_data['vol'])
-    end
 
-    redirect_to tickers_path
+    ticker_data = data['eos_cny']['ticker']
+    ticker.update(  ticker_id: data.keys[271],
+                    buy: ticker_data['buy'],
+                    sell: ticker_data['sell'],
+                    low: ticker_data['low'],
+                    high: ticker_data['high'],
+                    last: ticker_data['last'],
+                    vol: ticker_data['vol'])
 
+     redirect_to tickers_path
   end
 end
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -5,7 +5,7 @@ namespace :dev do
     data = JSON.parse(response.body)
     data.each do |key, value|
       existing_ticker = Ticker.find_by_ticker_id(key)
-      next unless existing_ticker.nil?
+      next unless existing_ticker.nil? #不存在就建立一个
       ticker_data = value['ticker']
       Ticker.create!( ticker_id: key,
                       at: value['at'],
@@ -14,7 +14,7 @@ namespace :dev do
                       low: ticker_data['low'],
                       high: ticker_data['high'],
                       last: ticker_data['last'],
-                      vol: ticker_data['vol'])
+                      vol: ticker_data['vol']) #请部分是你的数据库的值，=> 后面是你获取的数据的值
     end
     puts "Total: #{Ticker.count} tickers"
   end
