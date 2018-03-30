2c24980aec38db3ceb54b22fa10fd445d89b691f
liubingjie <breeder1121@gmail.com>
2018-03-30 12:39:50 +0800

    add tickers new

--- app/controllers/tickers_controller.rb
+++ app/controllers/tickers_controller.rb
@@ -20,21 +20,6 @@ class TickersController < ApplicationController
                     high: ticker_data['high'],
                     last: ticker_data['last'],
                     vol: ticker_data['vol'])
-    data.each do |key, value|
-      existing_ticker = Ticker.find_by_ticker_id(key)
-      next unless existing_ticker.nil? #不存在就建立一个
-      ticker_data = value['ticker']
-      Ticker.create!( ticker_id: key,
-                      at: value['at'],
-                      buy: ticker_data['buy'],
-                      sell: ticker_data['sell'],
-                      low: ticker_data['low'],
-                      high: ticker_data['high'],
-                      last: ticker_data['last'],
-                      vol: ticker_data['vol']) #请部分是你的数据库的值，=> 后面是你获取的数据的值
-    end
-
-
-     redirect_to ticker_path
+    redirect_to ticker_path
   end
 end
